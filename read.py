53bfdcd57a57207ad391e1089b608d47a4076bf5
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2018-05-23 19:32:45 +0200

    read.py dzieli na tokeny, usuwa punktuacje i koncowki

--- read.py
+++ read.py
@@ -1,50 +1,19 @@
 import os
-
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import EnglishStemmer
 
-stop = stopwords.words('english')
-
-srcript_dir = os.path.dirname(__file__)
+script_dir = os.path.dirname(__file__)
 snip_rel_path = "st/original_rt_snippets.txt"
-abs_file_path = os.path.join(srcript_dir, snip_rel_path)
+abs_file_path = os.path.join(script_dir, snip_rel_path)
 
-porter = PorterStemmer()
+stop_words = set(stopwords.words('english'))
+snowball_stemmer = EnglishStemmer()
 
-przyklad = 'runners like running and thus they run and it is fine for me'
-
-
-def tokenizer_portert(text):
-    return [porter.stem(word) for word in text.split()]
-
-
-print(tokenizer_portert(przyklad))  ## Tylko proba
-
-
-def tokenizer(text):
-    print(text.split())  ## Do debuggowania, zeby widziec co tam robi
-    return text.split()
-
-
-print(tokenizer(przyklad))  ## rowniez tylko do patrzenia co tu w trawie piszczy
-
-
-def erase_common_words(text):
-    tmp = [w for w in tokenizer_portert(text)
-           # tutaj było " [-10:] " ale nie mam zielonego pojęcia co to oznacza, po jej usunięciu działa lepiej? XDDDDD
-           if w not in stop]
-    print(tmp)  ## Debuggowanie
-    return tmp
-
-
-erase_common_words(przyklad)
-
-
-def get_tokens(file):
-    tokens = []
-    with open(file, 'r+') as f:
-        for line in f:
-            tokens.append(erase_common_words(line))
-    return sum(tokens, [])
-
-# print(get_tokens(abs_file_path))
+tokenized_sentences = []
+for line in open(abs_file_path, 'r+'):
+    tokens = word_tokenize(line)
+    filtered_sentence = [w.lower() for w in tokens
+                         if w not in stop_words and w.isalpha()]
+    tokenized_sentences.append(list(
+                                map(snowball_stemmer.stem, filtered_sentence)))
