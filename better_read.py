f9e3c933a866c228c559bbf94ed3ed05d6b1e1b3
pawciow <pawciow123@gmail.com>
2018-05-30 12:06:21 +0200

    Jest wektor z rzeczami, czyli miodzio

--- better_read.py
+++ better_read.py
@@ -1,6 +1,6 @@
 import os
 
-from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.preprocessing.text import Tokenizer
 
 srcript_dir = os.path.dirname(__file__)
 snip_rel_path = "zdania.txt"
@@ -21,6 +21,9 @@ print(T.word_counts)
 print('Ilosc linijek:')
 print(T.document_count)
 
+print('Indeks::')
+print(T.word_index)
+
 print('Slownik:')
 print(T.word_docs)
 
@@ -28,6 +31,7 @@ second_docs = open(abs_file_path, 'r+')
 
 docs_list = [line for line in second_docs]
 
-encoded_docs = [text_to_word_sequence(line, lower=False, split=" ") for line in docs_list]
-print('Zakdowane rzeczy:')
-# print(encoded_docs)
+encoded_docs = T.texts_to_matrix(docs_list, mode='count')
+# encoded_docs = [T.text_to_matrix(line, mode='count') for line in docs_list]
+# print('Zakdowane rzeczy:')
+print(encoded_docs)
