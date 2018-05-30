b440b5105e1fef690568f22bed8f6aa8c5827f55
pawciow <pawciow123@gmail.com>
2018-05-30 20:35:26 +0200

    prawie działa, już prawie ale jednak nie do końca

--- better_read.py
+++ better_read.py
@@ -1,38 +1,32 @@
 import os
 
+from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 
 srcript_dir = os.path.dirname(__file__)
 snip_rel_path = "zdania.txt"
 abs_file_path = os.path.join(srcript_dir, snip_rel_path)
 
-T = Tokenizer()
+T = Tokenizer(num_words=5000)
 docs = open(abs_file_path, 'r+')
 
 T.fit_on_texts(docs)
-
-# with open(abs_file_path, 'r+') as f:
-#   for line in f:
-#      T.fit_on_texts(line)
-
-print('Licznik slow:')
-print(T.word_counts)
-
-print('Ilosc linijek:')
-print(T.document_count)
-
-print('Indeks::')
-print(T.word_index)
-
-print('Slownik:')
-print(T.word_docs)
 
 second_docs = open(abs_file_path, 'r+')
 
 docs_list = [line for line in second_docs]
 
-encoded_docs = T.texts_to_matrix(docs_list, mode='count')
-# encoded_docs = [T.text_to_matrix(line, mode='count') for line in docs_list]
-# print('Zakdowane rzeczy:')
+encoded_docs = T.texts_to_sequences(docs_list)
+encoded_docs = pad_sequences(encoded_docs, maxlen=100)
+
 print(encoded_docs)
-print(str(len(encoded_docs)))
+
+snip_rel_path = "sentiment.txt"
+abs_file_path = os.path.join(srcript_dir, snip_rel_path)
+
+sentiments = open(abs_file_path, 'r+')
+sent_list = [line for line in sentiments]
+sent_list.extend(['0' for i in range(1, 125)])  # MAŁY PATCH XD
+
+# print(len(sent_list))
+# print(str(len(encoded_docs)))
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import numpy
-from keras.datasets import imdb
 from keras.layers import Dense, Flatten
 from keras.layers.embeddings import Embedding
 from keras.models import Sequential
-from keras.preprocessing import sequence
 
-# fixed seed
+import better_read
 
-seed = 7
-numpy.random.seed(seed)
+#
+# # fixed seed
+# seed = 7
+# numpy.random.seed(seed)
 top_words = 5000
-(X_train, Y_train), (X_test, Y_test) = imdb.load_data(num_words=top_words)
+# (X_train, Y_train), (X_test, Y_test) = imdb.load_data(num_words=top_words)
 
-max_words = 500
-X_train = sequence.pad_sequences(X_train, maxlen=max_words)
-X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+max_words = 100
+# X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+# X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+
+(X1_train, Y1_train), (X1_test, Y1_test) = (better_read.encoded_docs, better_read.sent_list), (
+better_read.encoded_docs, better_read.sent_list)
+
+print(X1_train.shape)
 
 model = Sequential()
 model.add(Embedding(top_words, 32, input_length=max_words))
@@ -25,8 +29,7 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
-model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=2,
-          batch_size=128, verbose=2)
+model.fit(X1_train, Y1_train, validation_data=(X1_test, Y1_test), epochs=2, verbose=2)
 
-scores = model.evaluate(X_test, Y_test, verbose=0)
+scores = model.evaluate(X1_test, Y1_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
