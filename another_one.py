62ce8878ca5d27031a064c18febb3470b7402340
pawciow <pawciow123@gmail.com>
2018-06-14 22:48:24 +0200

    IMDB version

--- another_one.py
+++ another_one.py
@@ -1,9 +1,9 @@
 import pandas as pd
+from keras.datasets import imdb
 from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
 from keras.models import Sequential
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
-from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('my_data.csv')
 data = data[['text', 'sentiment']]
@@ -13,23 +13,25 @@ tokenizer = Tokenizer(num_words=max_features, split=' ')
 tokenizer.fit_on_texts(data['text'].values)
 X = tokenizer.texts_to_sequences(data['text'].values)
 X = pad_sequences(X)
-
 embed_dim = 128
 lstm_out = 196
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+x_train = pad_sequences(x_train)
+x_test = pad_sequences(x_test)
 
 model = Sequential()
-model.add(Embedding(max_features, embed_dim, input_length=X.shape[1]))
+model.add(Embedding(max_features, embed_dim))
 model.add(SpatialDropout1D(0.4))
-model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(200, activation='sigmoid'))
-model.add(Dense(84, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+# model.add(Dense(200, activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
-Y = pd.get_dummies(data['sentiment'].values)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
-print(X_train.shape, Y_train.shape)
-print(X_test.shape, Y_test.shape)
+# Y = pd.get_dummies(data['sentiment'].values)
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+# print(X_train.shape, Y_train.shape)
+# print(X_test.shape, Y_test.shape)
 
 # batch_size = 10
 # X_train = X_train[:3000]
@@ -38,23 +40,16 @@ print(X_test.shape, Y_test.shape)
 # Y_test = Y_test[:3000]
 
 batch_size = 32
-model.fit(X_train, Y_train, epochs=50, batch_size=batch_size, verbose=2)
-
+model.fit(x_train, y_train, epochs=10, batch_size=batch_size, verbose=2)
 validation_size = 1500
-# validation_size = 5
-#validation_size = 1000
 
-X_validate = X_test[-validation_size:]
-Y_validate = Y_test[-validation_size:]
-X_test = X_test[:-validation_size]
-Y_test = Y_test[:-validation_size]
-# score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
-# print("score: %.2f" % (score))
-# print("acc: %.2f" % (acc))
-# print("Accuracy: %.2f%%" % (scores[1]*100))
+y_validate = x_test[-validation_size:]
+x_validate = y_test[-validation_size:]
+x_test = x_test[:-validation_size]
+x_test = y_test[:-validation_size]
 
-scores = model.evaluate(X_train, Y_train, batch_size=batch_size, verbose=0)
+scores = model.evaluate(x_train, y_train, batch_size=batch_size, verbose=0)
 print("Accuracy training: %.2f%%" % (scores[1] * 100))
 
-scores = model.evaluate(X_test, Y_test, batch_size=batch_size, verbose=0)
+scores = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
 print("Accuracy test: %.2f%%" % (scores[1] * 100))
