c41dc8eaf3ae0d70fd57849a51aac30b68d045cc
pawciow <pawciow123@gmail.com>
2018-06-15 08:05:26 +0200

    Final

--- another_one.py
+++ another_one.py
@@ -1,27 +1,24 @@
-import pandas as pd
 from keras.datasets import imdb
-from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from keras.layers import Dense, Embedding, LSTM
 from keras.models import Sequential
 from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
 
-data = pd.read_csv('my_data.csv')
-data = data[['text', 'sentiment']]
+# data = pd.read_csv('my_data.csv')
+# data = data[['text', 'sentiment']]
 
 max_features = 2000
-tokenizer = Tokenizer(num_words=max_features, split=' ')
-tokenizer.fit_on_texts(data['text'].values)
-X = tokenizer.texts_to_sequences(data['text'].values)
-X = pad_sequences(X)
+# tokenizer = Tokenizer(num_words=max_features, split=' ')
+# tokenizer.fit_on_texts(data['text'].values)
+# x = tokenizer.texts_to_sequences(data['text'].values)
+# x = pad_sequences(X)
 embed_dim = 128
-lstm_out = 196
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 x_train = pad_sequences(x_train)
 x_test = pad_sequences(x_test)
 
 model = Sequential()
 model.add(Embedding(max_features, embed_dim))
-model.add(SpatialDropout1D(0.4))
+# model.add(SpatialDropout1D(0.4))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
 # model.add(Dense(200, activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))
@@ -41,15 +38,15 @@ print(model.summary())
 
 batch_size = 32
 model.fit(x_train, y_train, epochs=10, batch_size=batch_size, verbose=2)
-validation_size = 1500
+# validation_size = 1500
 
-y_validate = x_test[-validation_size:]
-x_validate = y_test[-validation_size:]
-x_test = x_test[:-validation_size]
-x_test = y_test[:-validation_size]
+# y_validate = x_test[-validation_size:]
+# x_validate = y_test[-validation_size:]
+# x_test = x_test[:-validation_size]
+# x_test = y_test[:-validation_size]
 
-scores = model.evaluate(x_train, y_train, batch_size=batch_size, verbose=0)
-print("Accuracy training: %.2f%%" % (scores[1] * 100))
+# scores = model.evaluate(x_train, y_train, batch_size=batch_size, verbose=0)
+# print("Accuracy training: %.2f%%" % (scores[1] * 100))
 
 scores = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
 print("Accuracy test: %.2f%%" % (scores[1] * 100))
--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-from keras.layers import Dense, Flatten
+import numpy
+from keras.datasets import imdb
+from keras.layers import Dense, Flatten, SpatialDropout1D
 from keras.layers.embeddings import Embedding
 from keras.models import Sequential
 from keras.preprocessing.sequence import pad_sequences
 
-import better_read
-
-#
-# # fixed seed
-# seed = 7
-# numpy.random.seed(seed)
+seed = 7
+numpy.random.seed(seed)
 top_words = 5000
-# (X_train, Y_train), (X_test, Y_test) = imdb.load_data(num_words=top_words)
+(X1_train, Y1_train), (X1_test, Y1_test) = imdb.load_data(num_words=top_words)
 
 max_words = 500
-# X_train = sequence.pad_sequences(X_train, maxlen=max_words)
-# X_test = sequence.pad_sequences(X_test, maxlen=max_words)
-
-(X1_train, Y1_train), (X1_test, Y1_test) = (better_read.encoded_docs, better_read.sent_list), (
-better_read.encoded_docs, better_read.sent_list)
-
 X1_train = pad_sequences(X1_train, maxlen=max_words)
-X1_test = pad_sequences(X1_train, maxlen=max_words)
+X1_test = pad_sequences(X1_test, maxlen=max_words)
+
 
 print(X1_train.shape)
 
 model = Sequential()
 model.add(Embedding(top_words, 32, input_length=max_words))
+model.add(SpatialDropout1D(0.4))
 model.add(Flatten())
 model.add(Dense(250, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
